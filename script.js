eef426337de4d7f2849e3abbc3a73c75333c2643
femsmedivest-sys <femsmedivest@gmail.com>
2025-10-07 10:29:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -286,7 +286,7 @@ async function setupHospitalPage() {
         });
 
         if (Object.keys(locations).length === 0) {
-            mainContent.innerHTML += `<p style="text-align:center; color:red; font-weight:bold;">No data found for this system! 😲😤.</p>`;
+            mainContent.innerHTML += `<p style="text-align:center; color:red; font-weight:bold;">No data found for this system!.</p>`;
             return;
         }
 
@@ -359,4 +359,5 @@ if (window.location.pathname.endsWith('hospital-page.html')) {
     document.addEventListener('DOMContentLoaded', setupHospitalPage);
 } else if (window.location.pathname === '/' || window.location.pathname.endsWith('index.html')) {
     document.addEventListener('DOMContentLoaded', updateHospitalCards);
+
 }
