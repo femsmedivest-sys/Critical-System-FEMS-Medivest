86c44f3a2c5403fe5d742aa7fa4aefd2592f66fd
femsmedivest-sys <femsmedivest@gmail.com>
2025-11-14 11:10:16 +0800

    perubahan nama

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function() {
 const hospitalData = [
     // Pastikan URL di sini adalah URL Apps Script yang betul
     // Jika tiada URL, biarkan string kosong seperti ini: ''
-    //---N9-------
+    //-------------------------------N9----------------------------------
     { name: "Hospital Tuanku Ja'afar, Seremban", 
         id: "TUANKU-JAAFAR", sheetsUrl: "https://script.google.com/macros/s/AKfycbyURuTW-q5YGseD763PXLfWP198nBg-DLToJxPJ3FciH7pQ12gebbU0xL0WEcrhkHiH/exec" },
     { name: "Hospital Tuanku Ampuan Najihah, Kuala Pilah", 
